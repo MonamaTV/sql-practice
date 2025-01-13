f1d198491d4fc1aee31a49f3fc22d9d2cf158be6
Tadima <keletsovincent92@gmail.com>
2025-01-13 10:59:50 +0200

    learning sql

---
+++ Test/insert.sql
@@ -0,0 +1,16 @@
+USE LearningSQL;
+
+INSERT INTO Users(FirstName, LastName, Email, Password)
+VALUES ('Tadima', 'Monama', 'test0@gmail.com', '12323Tadima');
+
+
+INSERT INTO Users(FirstName, LastName, Email, Password)
+VALUES ('Tadima', 'Monama', 'test1@gmail.com', '12323Tadima');
+
+
+INSERT INTO Users(FirstName, LastName, Email, Password)
+VALUES ('Tadima', 'Monama', 'test2@gmail.com', '12323Tadima');
+
+
+INSERT INTO Users(FirstName, LastName, Email, Password)
+VALUES ('Tadima', 'Monama', 'test3@gmail.com', '12323Tadima');
